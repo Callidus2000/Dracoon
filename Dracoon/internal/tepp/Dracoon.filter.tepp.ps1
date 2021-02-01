508baa274739415e89383d7204fb1e12e5397d86
Callidus2000 <73584079+Callidus2000@users.noreply.github.com>
2021-02-01 12:21:02 +0100

    Comment added for new Dracoon.filter.tepp.ps1 workflow

--- Dracoon/internal/tepp/Dracoon.filter.tepp.ps1
+++ Dracoon/internal/tepp/Dracoon.filter.tepp.ps1
@@ -1,13 +1,19 @@
 ﻿Register-PSFTeppScriptblock -Name "Dracoon.filter" -ScriptBlock {
     try {
+        # Get inline help of configured command
         $help = get-help $commandName
+        # Get help for the parameter -Filter
         $parameterHelp = $Help.parameters.parameter | Where-Object Name -EQ 'Filter'
         $filterDescription = $parameterHelp.Description.Text
+        # Extract filter examples, format:
+        # 'attribute:operator:[possible Values]'
         $pattern = "'(\w*?):\[?([\w, \/]*?)\]?:\[?([\w, \/]*?)\]?',?"
         $results = $filterDescription | Select-String $pattern -AllMatches
         foreach ($match in $results.Matches) {
             $attribute = $match.Groups[1]
+            # Inline help may provide multiple operators, divided by '/'
             $operators = $match.Groups[2] -split '/'
+            # Inline help may provide multiple value examples, divided by '/'
             $valueExamples = $match.Groups[3] -split '/'
             foreach ($operator in $operators) {
                 foreach ($value in $valueExamples) {
